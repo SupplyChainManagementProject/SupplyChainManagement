daedd2c66247afa1132eb45f9335b67088e56769
Shagil Islam <shagilislam786@gmail.com>
2023-04-11 20:08:26 +0530

    Sepolia Test Net deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 // const { ethers } = require("hardhat");
 
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
 task("accounts", "Prints the list of accounts", async() => {
   const accounts = await ethers.getSigners();
@@ -12,6 +13,11 @@ task("accounts", "Prints the list of accounts", async() => {
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+    }
+  },
   solidity: "0.8.9",
-
-};
+}
